a2c359c7a52a7d700f38d8cfe9698eed870dd5e6
Patisson4 <mdpoluhin@gmail.com>
2022-10-11 16:22:49 +0300

    feat: open file dialog

--- YAPaint/ViewModels/MainWindowViewModel.cs
+++ YAPaint/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,34 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using ReactiveUI;
 
 namespace YAPaint.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public Image Greeting => default!;
+        private string _imagePath = string.Empty;
+
+        public string ImagePath
+        {
+            get => _imagePath;
+            set => this.RaiseAndSetIfChanged(ref _imagePath, value);
+        }
+
+        public async Task Open()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filters = new List<FileDialogFilter>
+                    { new FileDialogFilter { Name = "Image", Extensions = { "jpg", "png", "pnm", "bmp" } } },
+            };
+
+            string[] result = await dialog.ShowAsync(new Window());
+
+            if (result is not null)
+            {
+                ImagePath = result[0];
+            }
+        }
     }
 }
