76791cbb3113c1a136eac98bbdab17b8d37a18eb
Patisson4 <mdpoluhin@gmail.com>
2022-11-05 16:46:57 +0300

    feat: color channel class

---
+++ YAPaint/Models/Coefficient.cs
@@ -0,0 +1,37 @@
+﻿using YAPaint.Tools;
+
+namespace YAPaint.Models;
+
+public readonly record struct Coefficient
+{
+    private readonly float _value;
+
+    public Coefficient(float value)
+    {
+        CustomExceptionHelper.ThrowIfNotBetween(value, 0f, 1f);
+
+        _value = value;
+    }
+
+    public static Coefficient Zero { get; } = new Coefficient(0f);
+    
+    public static float Normalize(byte value)
+    {
+        return (float)value / byte.MaxValue;
+    }
+
+    public static byte Denormalize(float value)
+    {
+        return (byte)(value * byte.MaxValue);
+    }
+
+    public static implicit operator Coefficient(float value)
+    {
+        return new Coefficient(value);
+    }
+
+    public static implicit operator float(Coefficient coefficient)
+    {
+        return coefficient._value;
+    }
+}
