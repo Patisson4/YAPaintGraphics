28d9bb8b6e99f2703717f24fc91ac5053b9fee68
Patisson4 <mdpoluhin@gmail.com>
2022-11-05 16:45:48 +0300

    feat: exception helper

---
+++ YAPaint/Tools/CustomExceptionHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace YAPaint.Tools;
+
+public static class CustomExceptionHelper
+{
+    public static void ThrowIfGreaterThan(
+        int value,
+        int upperBound,
+        [CallerArgumentExpression("value")]
+        string paramName = null)
+    {
+        if (value > upperBound)
+        {
+            throw new ArgumentOutOfRangeException(paramName, "Value exceeds operating range");
+        }
+    }
+
+    public static void ThrowIfNotBetween(
+        float value,
+        float lowerBound,
+        float upperBound,
+        [CallerArgumentExpression("value")]
+        string paramName = null)
+    {
+        if (value < lowerBound || value > upperBound)
+        {
+            throw new ArgumentOutOfRangeException(paramName, "Value exceeds operating range");
+        }
+    }
+}
