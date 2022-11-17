113d712be2847a10e2418d2bbe19d90f77036eb1
vaner29 <gleb.mihailichencko@gmail.com>
2022-11-18 01:25:02 +0300

    tweak: change formula for YCbCr601

--- YAPaint/Models/ExtraColorSpaces/YCbCr601.cs
+++ YAPaint/Models/ExtraColorSpaces/YCbCr601.cs
@@ -37,18 +37,18 @@ public class YCbCr601 : IThreeChannelColorSpace, IColorSpace
     public Rgb ToRgb()
     {
         return new Rgb(
-            255f / 219f * (FirstChannel.Value * 255f - 16) + 255f / 224f * 1.402f * (ThirdChannel.Value * 255f - 128),
-            255f / 219f * (FirstChannel.Value * 255f - 16) - 255f / 224f * 1.772f * 0.114f / 0.587f * (SecondChannel.Value * 255f - 128) - 255f / 224f * 1.402f * 0.299f / 0.587f * (ThirdChannel.Value * 255f - 128),
-            255f / 219f * (FirstChannel.Value * 255f - 16) + 255f / 224f * 1.772f  * (SecondChannel.Value * 255f - 128));
+            FirstChannel.Value + 1.402f * ThirdChannel.Value,
+            FirstChannel.Value - (0.299f * 1.402f / 0.587f) * ThirdChannel.Value -
+            (0.114f * 1.772f / 0.587f) * SecondChannel.Value,
+            FirstChannel.Value + 1.772f * SecondChannel.Value);
     }
 
     public static IColorSpace FromRgb(Rgb color)
     {
-        return new YCbCr601((16 + 65.481f * color.FirstChannel.Value + 128.553f * color.SecondChannel.Value +
-                                   24.966f * color.ThirdChannel.Value) / 255f, 
-            (128 - 37.797f * color.FirstChannel.Value -74.203f * color.SecondChannel.Value +
-                     112f * color.ThirdChannel.Value) / 255f, 
-            (128 + 112f * color.FirstChannel.Value -93.786f * color.SecondChannel.Value -18.214f 
-                * color.ThirdChannel.Value) / 255f );
+        var y = 0.299f * color.FirstChannel.Value + 0.587f * color.SecondChannel.Value +
+                 0.114f * color.ThirdChannel.Value;
+        var cb = 0.5f - (color.ThirdChannel.Value - y) / 1.772f;
+        var cr = 0.5f + (color.FirstChannel.Value - y) / 1.402f;
+        return new YCbCr601( y, cb, cr);
     }
 }
