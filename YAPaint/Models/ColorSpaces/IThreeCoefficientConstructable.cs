fac4f1acdb8e35be9916843baadce57fda998a3e
Patisson4 <mdpoluhin@gmail.com>
2022-11-25 22:32:42 +0300

    feat: generic interfaces for operations

---
+++ YAPaint/Models/ColorSpaces/IThreeCoefficientConstructable.cs
@@ -0,0 +1,6 @@
+﻿namespace YAPaint.Models.ColorSpaces;
+
+public interface IThreeCoefficientConstructable<TSelf> where TSelf : IThreeCoefficientConstructable<TSelf>
+{
+    static abstract TSelf FromCoefficients(Coefficient first, Coefficient second, Coefficient third);
+}
