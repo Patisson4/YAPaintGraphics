94259a31e4d445dcbcebce88b96a321eaf472929
vaner29 <gleb.mihailichencko@gmail.com>
2022-11-25 15:55:25 +0300

    feat: add HSV colorspace

--- YAPaint/Models/ExtraColorSpaces/Hsv.cs
+++ YAPaint/Models/ExtraColorSpaces/Hsv.cs
@@ -1,6 +1,111 @@
-﻿using YAPaint.Models.ColorSpaces;
+﻿using System;
+using YAPaint.Models.ColorSpaces;
 
 namespace YAPaint.Models.ExtraColorSpaces;
 
-public class Hsv // : IThreeChannelColorSpace, IColorSpace
-{ }
+public class Hsv : IThreeChannelColorSpace, IColorSpace
+{
+    public ColorChannel FirstChannel { get; }
+    public ColorChannel SecondChannel { get; }
+    public ColorChannel ThirdChannel { get; }
+
+    public Hsv(Coefficient h, Coefficient s, Coefficient v)
+    {
+        FirstChannel = new ColorChannel(h);
+        SecondChannel = new ColorChannel(s);
+        ThirdChannel = new ColorChannel(v);
+    }
+
+    public byte[] ToRaw()
+    {
+        return new[]
+        {
+            Coefficient.Denormalize(FirstChannel.Value),
+            Coefficient.Denormalize(SecondChannel.Value),
+            Coefficient.Denormalize(ThirdChannel.Value),
+        };
+    }
+
+    public string ToPlain()
+    {
+        return
+            $"{Coefficient.Denormalize(FirstChannel.Value)} {Coefficient.Denormalize(SecondChannel.Value)} {Coefficient.Denormalize(ThirdChannel.Value)}";
+    }
+
+    public Rgb ToRgb()
+    {
+        var C = ThirdChannel.Value * SecondChannel.Value;
+        var H = FirstChannel.Value * 6f;
+        var X = C * (1 - Math.Abs(H % 2 - 1));
+        var m = ThirdChannel.Value - C;
+        float R1 = 0, G1 = 0, B1 = 0;
+        if (H is >= 0 and < 1)
+        {
+            R1 = C;
+            G1 = X;
+            B1 = 0;
+        }
+        else if (H is >= 1 and < 2)
+        {
+            R1 = X;
+            G1 = C;
+            B1 = 0;
+        }
+        else if (H is >= 2 and < 3)
+        {
+            R1 = 0;
+            G1 = C;
+            B1 = X;
+        }
+        else if (H is >= 3 and < 4)
+        {
+            R1 = 0;
+            G1 = X;
+            B1 = C;
+        }
+        else if (H is >= 4 and < 5)
+        {
+            R1 = X;
+            G1 = 0;
+            B1 = C;
+        }
+        else
+        {
+            R1 = C;
+            G1 = 0;
+            B1 = X;
+        }
+        
+        return new Rgb(R1 + m, G1 + m, B1 + m);
+    }
+
+    public static IColorSpace FromRgb(Rgb color)
+    {
+        var M = float.Max(float.Max(color.FirstChannel.Value, color.SecondChannel.Value), color.ThirdChannel.Value);
+        var m = float.Min(float.Min(color.FirstChannel.Value, color.SecondChannel.Value), color.ThirdChannel.Value);
+        var C = M - m;
+        float H = 0, S = 0, V = M;
+        if (C == 0)
+        {
+            H = 0;
+        }
+        else if (M == color.FirstChannel.Value)
+        {
+            H = (((color.SecondChannel.Value - color.ThirdChannel.Value) / C) % 6) / 6;
+        }
+        else if (M == color.SecondChannel.Value)
+        {
+            H = ((color.ThirdChannel.Value - color.FirstChannel.Value) / C + 2) / 6;
+        }
+        else
+        {
+            H = ((color.FirstChannel.Value - color.SecondChannel.Value) / C + 4) / 6;
+        }
+
+        if (V != 0)
+        {
+            S = C / V;
+        }
+        return new Hsv(H, S, V);
+    }
+}
