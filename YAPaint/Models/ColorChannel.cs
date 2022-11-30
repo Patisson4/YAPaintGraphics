142c74de25324514c5cbd7ddd20b6beb55caa592
Patisson4 <mdpoluhin@gmail.com>
2022-11-30 21:56:22 +0300

    Revert "chore: delete complicated abstractions"
    This reverts commit 1e1c65ee9690f4617c3d6938c49c9657921df607.

---
+++ YAPaint/Models/ColorChannel.cs
@@ -0,0 +1,14 @@
+﻿namespace YAPaint.Models;
+
+public class ColorChannel
+{
+    private readonly Coefficient _value;
+
+    public ColorChannel(Coefficient value)
+    {
+        _value = value;
+    }
+
+    public bool IsVisible { get; set; } = true;
+    public Coefficient Value => IsVisible ? _value : Coefficient.Zero;
+}
---
+++ YAPaint/Models/ColorSpaces/IColorSpace.cs
@@ -0,0 +1,10 @@
+﻿namespace YAPaint.Models.ColorSpaces;
+
+public interface IColorSpace
+{
+    byte[] ToRaw();
+    string ToPlain();
+
+    Rgb ToRgb();
+    static abstract IColorSpace FromRgb(Rgb color);
+}
