d3043d0b4603576c635b245919d5412ee2fd2750
Patisson4 <mdpoluhin@gmail.com>
2022-12-02 17:57:32 +0300

    feat: p2 and p5 format write check

--- YAPaint/Models/PortableBitmap.cs
+++ YAPaint/Models/PortableBitmap.cs
@@ -110,37 +110,113 @@ public class PortableBitmap
         _isThirdVisible = !_isThirdVisible;
     }
 
-    //TODO: P5 check
     public void SaveRaw(Stream stream)
     {
-        stream.Write(Encoding.ASCII.GetBytes("P6\n"));
-        stream.Write(Encoding.ASCII.GetBytes($"{Width} {Height}\n"));
-        stream.Write(Encoding.ASCII.GetBytes($"{byte.MaxValue}\n"));
+        int type = 6;
+        if (ColorConverter is BlackAndWhite or GreyScale)
+        {
+            type = 5;
+        }
+        else if (_isFirstVisible && !_isSecondVisible && !_isThirdVisible
+              || !_isFirstVisible && _isSecondVisible && !_isThirdVisible
+              || !_isFirstVisible && !_isSecondVisible && _isThirdVisible)
+        {
+            type = 5;
+        }
+
+        bool byPart = type == 5;
+        WriteHeader(stream, type, byte.MaxValue);
 
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width; x++)
             {
-                stream.Write(GetPixel(x, y).ToRaw());
+                WriteBytePixel(stream, GetPixel(x, y), byPart);
             }
         }
     }
 
-    //TODO: P2 check
     public void SavePlain(Stream stream)
     {
-        stream.Write(Encoding.ASCII.GetBytes("P3\n"));
-        stream.Write(Encoding.ASCII.GetBytes($"{Width} {Height}\n"));
-        stream.Write(Encoding.ASCII.GetBytes($"{byte.MaxValue}\n"));
+        int type = 3;
+        if (ColorConverter is BlackAndWhite or GreyScale)
+        {
+            type = 2;
+        }
+        else if (_isFirstVisible && !_isSecondVisible && !_isThirdVisible
+              || !_isFirstVisible && _isSecondVisible && !_isThirdVisible
+              || !_isFirstVisible && !_isSecondVisible && _isThirdVisible)
+        {
+            type = 2;
+        }
+
+        bool byPart = type == 2;
+        WriteHeader(stream, type, byte.MaxValue);
 
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width - 1; x++)
             {
-                stream.Write(Encoding.ASCII.GetBytes($"{GetPixel(x, y).ToPlain()} "));
+                WritePlainPixel(stream, GetPixel(x, y), byPart);
+                stream.Write(Encoding.ASCII.GetBytes(" "));
             }
 
-            stream.Write(Encoding.ASCII.GetBytes($"{GetPixel(Width - 1, y).ToPlain()}\n"));
+            WritePlainPixel(stream, GetPixel(Width - 1, y), byPart);
+            stream.Write(Encoding.ASCII.GetBytes("\n"));
+        }
+    }
+
+    private void WriteHeader(Stream stream, int type, int depth = 0)
+    {
+        stream.Write(Encoding.ASCII.GetBytes($"P{type}\n"));
+        stream.Write(Encoding.ASCII.GetBytes($"{Width} {Height}\n"));
+        stream.Write(Encoding.ASCII.GetBytes($"{depth}\n"));
+    }
+
+    private void WriteBytePixel(Stream stream, ColorSpace pixel, bool byPart = true)
+    {
+        var bytePixel = pixel.ToRaw();
+        if (!byPart)
+        {
+            stream.Write(bytePixel);
+        }
+        else
+        {
+            if (ColorConverter is BlackAndWhite or GreyScale || _isFirstVisible)
+            {
+                stream.WriteByte(bytePixel[0]);
+            }
+            else if (_isSecondVisible)
+            {
+                stream.WriteByte(bytePixel[1]);
+            }
+            else
+            {
+                stream.WriteByte(bytePixel[2]);
+            }
+        }
+    }
+
+    private void WritePlainPixel(Stream stream, ColorSpace pixel, bool byPart = true)
+    {
+        if (!byPart)
+        {
+            stream.Write(Encoding.ASCII.GetBytes($"{pixel.ToPlain()}"));
+        }
+        else
+        {
+            if (ColorConverter is BlackAndWhite or GreyScale || _isFirstVisible)
+            {
+                stream.Write(Encoding.ASCII.GetBytes($"{Coefficient.Denormalize(pixel.First)}"));
+            }
+            else if (_isSecondVisible)
+            {
+                stream.Write(Encoding.ASCII.GetBytes($"{Coefficient.Denormalize(pixel.Second)}"));
+            }
+            else
+            {
+                stream.Write(Encoding.ASCII.GetBytes($"{Coefficient.Denormalize(pixel.Third)}"));
+            }
         }
     }
 }
