e20b57edc866f7711a4b172be6e1dfeb602adcff
Patisson4 <mdpoluhin@gmail.com>
2022-12-04 05:48:57 +0300

    chore: docs

--- YAPaint/Models/ExtraColorSpaces/IColorConverter.cs
+++ YAPaint/Models/ExtraColorSpaces/IColorConverter.cs
@@ -2,6 +2,9 @@
 
 namespace YAPaint.Models.ExtraColorSpaces;
 
+/// <summary>
+/// Interface used only to distinguish three channeled ColorSpace converters from one channeled ones
+/// </summary>
 public interface IColorConverter : IColorBaseConverter
 {
 }
