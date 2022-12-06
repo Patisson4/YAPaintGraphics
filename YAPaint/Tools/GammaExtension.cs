7db50eee574e1a9280fa0078eab4b6aa0115b99e
Patisson4 <mdpoluhin@gmail.com>
2022-12-06 18:20:24 +0300

    feat: srgb gamma

--- YAPaint/Tools/GammaExtension.cs
+++ YAPaint/Tools/GammaExtension.cs
@@ -22,9 +22,46 @@ public static class GammaExtension
 
     private static ColorSpace WithGamma(this ref ColorSpace color, float value)
     {
+        if (value == 0)
+        {
+            return new ColorSpace(
+                ComputeSRgbGamma(color.First),
+                ComputeSRgbGamma(color.Second),
+                ComputeSRgbGamma(color.Third));
+        }
+
+        if (float.IsPositiveInfinity(value))
+        {
+            return new ColorSpace(
+                ComputeInverseSRgbGamma(color.First),
+                ComputeInverseSRgbGamma(color.Second),
+                ComputeInverseSRgbGamma(color.Third));
+            
+        }
+
         return new ColorSpace(
             float.Pow(color.First, value),
             float.Pow(color.Second, value),
             float.Pow(color.Third, value));
+    }
+
+    private static float ComputeSRgbGamma(float value)
+    {
+        if (value > 0.0031308f)
+        {
+            return 1.055f * float.Pow(value, 1 / 2.4f) - 0.055f;
+        }
+
+        return value * 12.92f;
+    }
+
+    private static float ComputeInverseSRgbGamma(float value)
+    {
+        if (value > 0.04045f)
+        {
+            return float.Pow((value + 0.055f) / 1.055f, 2.4f);
+        }
+
+        return value / 12.92f;
     }
 }
