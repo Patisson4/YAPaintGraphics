8700d2871be54bdd9f3d8c37d8e7f585a3e16925
Patisson4 <mdpoluhin@gmail.com>
2022-12-18 00:39:35 +0300

    chore: remove and refactor unused methods

--- YAPaint/Tools/PnmParser.cs
+++ YAPaint/Tools/PnmParser.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Drawing;
+using System.Globalization;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using YAPaint.Models;
 using AvaloniaBitmap = Avalonia.Media.Imaging.Bitmap;
 
@@ -21,13 +19,13 @@ public static class PnmParser
 
         char type = reader.ReadChar();
 
-        var width = GetNextHeaderValue(reader);
-        var height = GetNextHeaderValue(reader);
+        var width = GetNextTextValue(reader);
+        var height = GetNextTextValue(reader);
         var scale = 1;
 
         if (type is not ('1' or '4'))
         {
-            scale = GetNextHeaderValue(reader);
+            scale = GetNextTextValue(reader);
         }
 
         var map = new ColorSpace[width, height];
@@ -43,43 +41,6 @@ public static class PnmParser
         MyFileLogger.Log("DBG", $"Read file at {MyFileLogger.SharedTimer.Elapsed.TotalSeconds} s");
 
         return map;
-    }
-
-    public static async Task WriteTextImage(this Bitmap bitmap, string path)
-    {
-        await using var file = new StreamWriter(path);
-        await file.WriteLineAsync($"P3\n{bitmap.Width} {bitmap.Height}\n255");
-
-        for (int y = 0; y < bitmap.Height; y++)
-        {
-            for (int x = 0; x < bitmap.Width - 1; x++)
-            {
-                Color color = bitmap.GetPixel(x, y);
-                await file.WriteAsync($"{color.R} {color.G} {color.B} ");
-            }
-
-            {
-                Color color = bitmap.GetPixel(bitmap.Width - 1, y);
-                await file.WriteLineAsync($"{color.R} {color.G} {color.B}");
-            }
-        }
-    }
-
-    public static async Task WriteRawImage(this Bitmap bitmap, string path)
-    {
-        await using FileStream file = File.OpenWrite(path);
-        await file.WriteAsync(Encoding.ASCII.GetBytes($"P6\n{bitmap.Width} {bitmap.Height}\n255\n"));
-
-        for (int y = 0; y < bitmap.Height; y++)
-        {
-            for (int x = 0; x < bitmap.Width; x++)
-            {
-                Color color = bitmap.GetPixel(x, y);
-                file.WriteByte(color.R);
-                file.WriteByte(color.G);
-                file.WriteByte(color.B);
-            }
-        }
     }
 
     private static ColorSpace ReadColor(BinaryReader reader, char type, int scale)
@@ -138,13 +99,18 @@ public static class PnmParser
         return new ColorSpace(Coefficient.Normalize(red), Coefficient.Normalize(green), Coefficient.Normalize(blue));
     }
 
-    private static int GetNextHeaderValue(BinaryReader reader)
+    private static int GetNextTextValue(BinaryReader reader)
     {
         bool hasValue = false;
         string value = string.Empty;
 
-        do
+        while (!hasValue)
         {
+            if (reader.PeekChar() == -1)
+            {
+                break;
+            }
+
             var c = reader.ReadChar();
 
             switch (c)
@@ -152,33 +118,14 @@ public static class PnmParser
                 case '\n' or '\r' or ' ' or '\t' when value.Length != 0:
                     hasValue = true;
                     break;
-                case >= '0' and <= '9':
+                case '\n' or '\r' or ' ' or '\t':
+                    break;
+                default:
                     value += c;
                     break;
             }
-        } while (!hasValue);
+        }
 
-        return int.Parse(value);
-    }
-
-    private static int GetNextTextValue(BinaryReader reader)
-    {
-        string value = string.Empty;
-        var c = reader.ReadChar();
-
-        do
-        {
-            value += c;
-            try
-            {
-                c = reader.ReadChar();
-            }
-            catch (EndOfStreamException)
-            {
-                break;
-            }
-        } while (c is not ('\n' or '\r' or ' ' or '\t') || value.Length == 0);
-
-        return int.Parse(value);
+        return int.Parse(value, CultureInfo.InvariantCulture);
     }
 }
