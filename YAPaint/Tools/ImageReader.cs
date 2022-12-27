4920a1ff0e961391b1b88b084a0bccd7d477db09
Patisson4 <mdpoluhin@gmail.com>
2022-12-27 21:26:41 +0300

    chore: refactors to support png read

---
+++ YAPaint/Tools/ImageReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using YAPaint.Models;
+
+namespace YAPaint.Tools;
+
+public static class ImageReader
+{
+    //ArrayPool returns more memory than necessary, therefore SequenceEqual's length check fails  
+    private static readonly byte[] Signature = new byte[8];
+
+    public static ColorSpace[,] ReadImage(Stream stream)
+    {
+        byte firstByte = (byte)stream.ReadByte();
+        if (firstByte == "P"u8[0])
+        {
+            return PnmParser.ReadImage(stream);
+        }
+
+        Signature[0] = firstByte;
+        if (stream.Read(Signature, 1, 7) == 7 && PngConverter.IsValidSignature(Signature))
+        {
+            return PngConverter.ReadPng(stream);
+        }
+
+        throw new NotSupportedException("Unknown format specification");
+    }
+}
