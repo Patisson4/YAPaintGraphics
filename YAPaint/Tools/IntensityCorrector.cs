3cbbbec867b06a580bf702f04ded231183ce1828
Patisson4 <mdpoluhin@gmail.com>
2022-12-30 01:26:11 +0300

    chore: refactor IntensityCorrector

--- YAPaint/Tools/IntensityCorrector.cs
+++ YAPaint/Tools/IntensityCorrector.cs
@@ -1,62 +1,64 @@
-﻿using System;
-using YAPaint.Models;
+﻿using YAPaint.Models;
 
 namespace YAPaint.Tools;
 
-public class IntensityCorrector
+public static class IntensityCorrector
 {
-    public static PortableBitmap CorrectIntensity(PortableBitmap bitmap, double ignoreProportion, double[][] histograms)
+    public static void CorrectIntensity(ref PortableBitmap bitmap, double proportion)
     {
+        (float leftEdge, float rightEdge) = FindEdges(bitmap, proportion);
+
+        for (int y = 0; y < bitmap.Height; y++)
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                var pixel = bitmap.GetPixel(x, y);
+                var newPixelFirst = float.Clamp((pixel.First - leftEdge) / (rightEdge - leftEdge), 0, 1);
+                var newPixelSecond = float.Clamp((pixel.Second - leftEdge) / (rightEdge - leftEdge), 0, 1);
+                var newPixelThird = float.Clamp((pixel.Third - leftEdge) / (rightEdge - leftEdge), 0, 1);
+                var newPixel = new ColorSpace(newPixelFirst, newPixelSecond, newPixelThird);
+                bitmap.SetPixel(x, y, newPixel);
+            }
+        }
+    }
+
+    private static (float leftEdge, float rightEdge) FindEdges(PortableBitmap bitmap, double proportion)
+    {
+        var histograms = BarGrapher.CreateBarGraphs(bitmap);
         var currentBack = 0d;
         int i;
-        var threshold = ignoreProportion * bitmap.Width * bitmap.Height * 3;
+        var threshold = proportion * bitmap.Width * bitmap.Height * 3;
         for (i = 0; i < 256; i++)
         {
-            for (var c = 0; c < 3; c++)
+            for (int c = 0; c < 3; c++)
             {
                 currentBack += histograms[c][i];
             }
+
             if (currentBack >= threshold)
                 break;
         }
 
         if (i >= 256)
             i = 255;
-        double leftEdge = i / 255d;
-        Console.WriteLine("leftedge:" + leftEdge);
-        
-        currentBack = threshold;
+        float leftEdge = i / 255f;
+
+        currentBack = 0;
         for (i = 255; i >= 0; i--)
         {
             for (int c = 0; c < 3; c++)
             {
-                currentBack -= histograms[c][i];
+                currentBack += histograms[c][i];
             }
-            if (currentBack <= 0)
+
+            if (currentBack >= threshold)
                 break;
         }
 
         if (i < 0)
             i = 0;
-        double rightEdge = i / 255d;
-        Console.WriteLine("rightedge:" + rightEdge);
-        
-        for (var y = 0; y < bitmap.Height; y++)
-        {
-            for (var x = 0; x < bitmap.Width; x++)
-            {
-                var pixel = bitmap.GetPixel(x, y);
-                var newPixelFirst = Coefficient.Normalize(Coefficient.Denormalize((float)Math.Clamp((pixel.First - leftEdge) / (rightEdge - leftEdge), 0d, 1.0d)));
-                var newPixelSecond = Coefficient.Normalize(Coefficient.Denormalize((float)Math.Clamp((pixel.Second - leftEdge) / (rightEdge - leftEdge), 0d,
-                    1.0d)));
-                var newPixelThird = Coefficient.Normalize(Coefficient.Denormalize((float)Math.Clamp((pixel.Third - leftEdge) / (rightEdge - leftEdge), 0d, 1.0d)));
-                var newPixel = new ColorSpace(newPixelFirst, newPixelSecond, newPixelThird);
-                bitmap.SetPixel(x, y, newPixel);
-            }
-        }
+        float rightEdge = i / 255f;
 
-        return bitmap;
+        return (leftEdge, rightEdge);
     }
-
-
 }
