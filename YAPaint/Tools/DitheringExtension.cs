26debea6d394bc0b2c922dbcd2789394656b1b04
Patisson4 <mdpoluhin@gmail.com>
2023-01-07 07:16:41 +0300

    fix: filters p1

--- YAPaint/Tools/DitheringExtension.cs
+++ YAPaint/Tools/DitheringExtension.cs
@@ -17,13 +17,16 @@ public static class DitheringExtension
         { 42, 26, 38, 22, 41, 25, 37, 21 },
     };
 
+    //WRN: doesn't work; TODO: fix
     public static PortableBitmap DitherOrdered(this PortableBitmap bitmap, int bitDepth)
     {
+        var oldBitmap = bitmap;
+        var newBitmap = bitmap;
         for (int y = 0; y < bitmap.Height; y++)
         {
             for (int x = 0; x < bitmap.Width; x++)
             {
-                ColorSpace currentPixel = bitmap.GetPixel(x, y);
+                ColorSpace currentPixel = oldBitmap.GetPixel(x, y);
                 int r = Coefficient.Denormalize(currentPixel.First);
                 int g = Coefficient.Denormalize(currentPixel.Second);
                 int b = Coefficient.Denormalize(currentPixel.Third);
@@ -44,89 +47,118 @@ public static class DitheringExtension
                 int gError = g - quantizedG;
                 int bError = b - quantizedB;
 
-                PropagateError(bitmap, x + 1, y, rError, gError, bError, 7 / 16.0f);
-                PropagateError(bitmap, x - 1, y + 1, rError, gError, bError, 3 / 16.0f);
-                PropagateError(bitmap, x, y + 1, rError, gError, bError, 5 / 16.0f);
-                PropagateError(bitmap, x + 1, y + 1, rError, gError, bError, 1 / 16.0f);
+                PropagateError(newBitmap, x + 1, y, rError, gError, bError, 7 / 16.0f);
+                PropagateError(newBitmap, x - 1, y + 1, rError, gError, bError, 3 / 16.0f);
+                PropagateError(newBitmap, x, y + 1, rError, gError, bError, 5 / 16.0f);
+                PropagateError(newBitmap, x + 1, y + 1, rError, gError, bError, 1 / 16.0f);
             }
         }
 
-        return bitmap;
+        return newBitmap;
     }
 
-    public static PortableBitmap DitherRandom(this PortableBitmap bitmap, int bitDepth)
+    public static PortableBitmap DitherRandom(this PortableBitmap bitmap)
     {
+        var newMap = new ColorSpace[bitmap.Width, bitmap.Height];
+
+        for (int i = 0; i < bitmap.Width; i++)
+        {
+            for (int j = 0; j < bitmap.Height; j++)
+            {
+                newMap[i, j] = bitmap.GetPixel(i, j);
+            }
+        }
+
         for (int y = 0; y < bitmap.Height; y++)
         {
             for (int x = 0; x < bitmap.Width; x++)
             {
-                ColorSpace currentPixel = bitmap.GetPixel(x, y);
-                int r = Coefficient.Denormalize(currentPixel.First);
-                int g = Coefficient.Denormalize(currentPixel.Second);
-                int b = Coefficient.Denormalize(currentPixel.Third);
+                ColorSpace color = bitmap.GetPixel(x, y);
+                double grayscale = bitmap.ColorConverter.GetGrayValue(color);
+                float threshold = Random.Shared.NextSingle();
 
-                r += Random.Shared.Next(-128, 128);
-                g += Random.Shared.Next(-128, 128);
-                b += Random.Shared.Next(-128, 128);
-
-                r = int.Clamp(r, 0, 255);
-                g = int.Clamp(g, 0, 255);
-                b = int.Clamp(b, 0, 255);
-
-                int quantizedR = (int)Math.Round(r / (float)((1 << bitDepth) - 1));
-                int quantizedG = (int)Math.Round(g / (float)((1 << bitDepth) - 1));
-                int quantizedB = (int)Math.Round(b / (float)((1 << bitDepth) - 1));
-
-                int rError = r - quantizedR;
-                int gError = g - quantizedG;
-                int bError = b - quantizedB;
-
-                PropagateError(bitmap, x + 1, y, rError, gError, bError, 7 / 16.0f);
-                PropagateError(bitmap, x - 1, y + 1, rError, gError, bError, 3 / 16.0f);
-                PropagateError(bitmap, x, y + 1, rError, gError, bError, 5 / 16.0f);
-                PropagateError(bitmap, x + 1, y + 1, rError, gError, bError, 1 / 16.0f);
+                if (grayscale < threshold)
+                {
+                    newMap[x, y] = bitmap.ColorConverter.Black;
+                }
+                else
+                {
+                    newMap[x, y] = bitmap.ColorConverter.White;
+                }
             }
         }
 
-        return bitmap;
+        return new PortableBitmap(newMap, bitmap.ColorConverter);
     }
 
     public static PortableBitmap DitherFloydSteinberg(this PortableBitmap bitmap, int bitDepth)
     {
+        var newMap = new ColorSpace[bitmap.Width, bitmap.Height];
+        var bufferMap = new (float, float, float)[bitmap.Width, bitmap.Height];
+
+        for (int i = 0; i < bitmap.Width; i++)
+        {
+            for (int j = 0; j < bitmap.Height; j++)
+            {
+                ColorSpace pixel = bitmap.GetPixel(i, j);
+                bufferMap[i, j] = (pixel.First, pixel.Second, pixel.Third);
+            }
+        }
+
         for (int y = 0; y < bitmap.Height; y++)
         {
             for (int x = 0; x < bitmap.Width; x++)
             {
-                ColorSpace currentPixel = bitmap.GetPixel(x, y);
-                int r = Coefficient.Denormalize(currentPixel.First);
-                int g = Coefficient.Denormalize(currentPixel.Second);
-                int b = Coefficient.Denormalize(currentPixel.Third);
+                newMap[x, y] = Quantize(ref bufferMap[x, y], (1 << bitDepth) - 1);
 
-                int quantizedR = (int)float.Round(r / (float)((1 << bitDepth) - 1));
-                int quantizedG = (int)float.Round(g / (float)((1 << bitDepth) - 1));
-                int quantizedB = (int)float.Round(b / (float)((1 << bitDepth) - 1));
+                float errorFirst = bufferMap[x, y].Item1 - newMap[x, y].First;
+                float errorSecond = bufferMap[x, y].Item2 - newMap[x, y].Second;
+                float errorThird = bufferMap[x, y].Item3 - newMap[x, y].Third;
 
-                int rError = r - quantizedR;
-                int gError = g - quantizedG;
-                int bError = b - quantizedB;
+                if (x + 1 < bitmap.Width)
+                {
+                    bufferMap[x + 1, y].Item1 += 7 / 16f * errorFirst;
+                    bufferMap[x + 1, y].Item2 += 7 / 16f * errorSecond;
+                    bufferMap[x + 1, y].Item3 += 7 / 16f * errorThird;
+                }
 
-                PropagateError(bitmap, x + 1, y, rError, gError, bError, 7 / 16.0f);
-                PropagateError(bitmap, x - 1, y + 1, rError, gError, bError, 3 / 16.0f);
-                PropagateError(bitmap, x, y + 1, rError, gError, bError, 5 / 16.0f);
-                PropagateError(bitmap, x + 1, y + 1, rError, gError, bError, 1 / 16.0f);
+                if (y + 1 >= bitmap.Height)
+                {
+                    continue;
+                }
+
+                if (x - 1 >= 0)
+                {
+                    bufferMap[x - 1, y + 1].Item1 += 3 / 16f * errorFirst;
+                    bufferMap[x - 1, y + 1].Item2 += 3 / 16f * errorSecond;
+                    bufferMap[x - 1, y + 1].Item3 += 3 / 16f * errorThird;
+                }
+
+                bufferMap[x, y + 1].Item1 += 5 / 16f * errorFirst;
+                bufferMap[x, y + 1].Item2 += 5 / 16f * errorSecond;
+                bufferMap[x, y + 1].Item3 += 5 / 16f * errorThird;
+
+                if (x + 1 < bitmap.Width)
+                {
+                    bufferMap[x + 1, y + 1].Item1 += 1 / 16f * errorFirst;
+                    bufferMap[x + 1, y + 1].Item2 += 1 / 16f * errorSecond;
+                    bufferMap[x + 1, y + 1].Item3 += 1 / 16f * errorThird;
+                }
             }
         }
 
-        return bitmap;
+        return new PortableBitmap(newMap, bitmap.ColorConverter);
     }
 
+    //WRN: doesn't work; TODO: fix
     public static PortableBitmap DitherAtkinson(this PortableBitmap bitmap, int bitDepth)
     {
         for (int y = 0; y < bitmap.Height; y++)
         {
             for (int x = 0; x < bitmap.Width; x++)
             {
-                ColorSpace currentPixel = bitmap.GetPixel(x, y);
+                var oldBitmap = bitmap;
+                ColorSpace currentPixel = oldBitmap.GetPixel(x, y);
                 int r = Coefficient.Denormalize(currentPixel.First);
                 int g = Coefficient.Denormalize(currentPixel.Second);
                 int b = Coefficient.Denormalize(currentPixel.Third);
@@ -151,13 +183,24 @@ public static class DitheringExtension
         return bitmap;
     }
 
+    private static ColorSpace Quantize(ref (float, float, float) color, int maxValue)
+    {
+        return new ColorSpace
+        {
+            First = float.Round(color.Item1 * maxValue) / maxValue,
+            Second = float.Round(color.Item2 * maxValue) / maxValue,
+            Third = float.Round(color.Item3 * maxValue) / maxValue,
+        };
+    }
+
+    [Obsolete("Implement your own boundary check in-place")]
     private static void PropagateError(
         PortableBitmap bitmap,
         int x,
         int y,
-        int rError,
-        int gError,
-        int bError,
+        float rError,
+        float gError,
+        float bError,
         float factor)
     {
         if (x < 0 || x >= bitmap.Width || y < 0 || y >= bitmap.Height)
@@ -165,11 +208,12 @@ public static class DitheringExtension
             return;
         }
 
+        var oldPixel = bitmap.GetPixel(x, y);
         var newPixel = new ColorSpace
         {
-            First = Coefficient.Normalize((int)Math.Round(rError * factor)),
-            Second = Coefficient.Normalize((int)Math.Round(gError * factor)),
-            Third = Coefficient.Normalize((int)Math.Round(bError * factor)),
+            First = Math.Clamp(oldPixel.First + rError * factor, 0, 1),
+            Second = Math.Clamp(oldPixel.Second + gError * factor, 0, 1),
+            Third = Math.Clamp(oldPixel.Third + bError * factor, 0, 1),
         };
 
         bitmap.SetPixel(x, y, newPixel);
