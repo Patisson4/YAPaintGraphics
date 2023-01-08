e81c2aaf367b84c7b7495a3a9b40e0491d401c0c
Patisson4 <mdpoluhin@gmail.com>
2023-01-08 08:35:01 +0300

    chore: LineDrawer refactor

--- YAPaint/Tools/LineDrawer.cs
+++ YAPaint/Tools/LineDrawer.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using YAPaint.Models;
+﻿using YAPaint.Models;
 
 namespace YAPaint.Tools;
 
 public static class LineDrawer
 {
     public static PortableBitmap DrawLine(
-        this PortableBitmap image,
+        this PortableBitmap bitmap,
         ColorSpace color,
-        float thickness,
+        int thickness,
         float transparency,
         (int x, int y) start,
         (int x, int y) end)
     {
         int dx = end.x - start.x;
         int dy = end.y - start.y;
-        
+
         int x1 = start.x;
         int y1 = start.y;
         int x2 = end.x;
         int y2 = end.y;
 
-        bool steep = Math.Abs(dy) > Math.Abs(dx);
+        bool steep = float.Abs(dy) > float.Abs(dx);
         if (steep)
         {
             int t = x1;
@@ -43,54 +42,60 @@ public static class LineDrawer
         }
 
         int dx1 = x2 - x1;
-        int dy1 = Math.Abs(y2 - y1);
+        int dy1 = int.Abs(y2 - y1);
         int err = dx1 / 2;
-        int ystep = y1 < y2 ? 1 : -1;
+        int step = y1 < y2 ? 1 : -1;
         int y = y1;
 
         for (int x = x1; x <= x2; x++)
         {
             int centerX = steep ? y : x;
             int centerY = steep ? x : y;
-            DrawCircle(image, color, thickness, transparency, centerX, centerY);
+            bitmap = DrawCircle(bitmap, color, thickness, transparency, centerX, centerY);
 
             err -= dy1;
-            if (err < 0)
+            if (err >= 0)
             {
-                y += ystep;
-                err += dx1;
+                continue;
             }
+
+            y += step;
+            err += dx1;
         }
 
-        return image;
+        return bitmap;
     }
-    
-    private static void DrawCircle(
-        PortableBitmap image,
+
+    private static PortableBitmap DrawCircle(
+        PortableBitmap bitmap,
         ColorSpace color,
-        float thickness,
+        int thickness,
         float transparency,
         int centerX,
         int centerY)
     {
-        int radius = (int)thickness / 2;
+        int radius = thickness / 2;
         for (int x = centerX - radius; x <= centerX + radius; x++)
         {
             for (int y = centerY - radius; y <= centerY + radius; y++)
             {
-                if (x >= 0 && x < image.Width && y >= 0 && y < image.Height)
+                if (x < 0 || x >= bitmap.Width || y < 0 || y >= bitmap.Height)
                 {
-                    ColorSpace pixelColor = image.GetPixel(x, y);
-                    byte[] pixelRaw = pixelColor.ToRaw();
-                    byte[] colorRaw = color.ToRaw();
-                    byte[] finalRaw = new byte[3];
-                    for (int i = 0; i < 3; i++)
-                    {
-                        finalRaw[i] = (byte)((1 - transparency) * pixelRaw[i] + transparency * colorRaw[i]);
-                    }
-                    ColorSpace finalColor = new ColorSpace(finalRaw[0], finalRaw[1], finalRaw[2]);
+                    continue;
                 }
+
+                ColorSpace pixelColor = bitmap.GetPixel(x, y);
+                var finalColor = new ColorSpace
+                {
+                    First = (1 - transparency) * pixelColor.First + transparency * color.First,
+                    Second = (1 - transparency) * pixelColor.Second + transparency * color.Second,
+                    Third = (1 - transparency) * pixelColor.Third + transparency * color.Third,
+                };
+
+                bitmap.SetPixel(x, y, finalColor);
             }
         }
+
+        return bitmap;
     }
 }
